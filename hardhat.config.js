2799ba1b2efe85acabede23c8f2c66dc5f05ba90
davidalfredostrowski <davidaostrowski@gmail.com>
2023-06-30 22:38:30 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,27 @@ module.exports = {
 paths: {
     artifacts: "./src/artifacts",
   },
+        networks: {
+                localhost: {
+                        url: "http://ec2-18-246-50-136.us-west-2.compute.amazonaws.com:8545"
+            ,           chainID: 1337
+
+                }
+,
+                hardhat: {
+                        chainID: 1337
+           }
+   },
+
+        solidity: "0.8.18",
+};
+/**  require("@nomicfoundation/hardhat-toolbox"); **/
+
+/** @type import('hardhat/config').HardhatUserConfig */
+/** module.exports = {
+paths: {
+    artifacts: "./src/artifacts",
+  },
 
 
 
@@ -17,3 +38,4 @@ paths: {
 
 	solidity: "0.8.18",
 };
+**/
