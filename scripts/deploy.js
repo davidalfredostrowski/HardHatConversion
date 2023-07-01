b5cacc10b3c29cea58303b0e6490842d53edc83a
davidalfredostrowski <davidaostrowski@gmail.com>
2023-07-01 16:54:57 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,29 +3,13 @@
 const hre = require("hardhat")
 
 async function main() {
-//  const [deployer] = await ethers.getSigners();
-
-
-
-//  console.log("Deploying contracts with the account:", deployer.address);
- // console.log("Account balance:", (await deployer.getBalance()).toString());
-
-
-  // Get the ContractFactories and Signers here.
-  const HELLO  = await hre.ethers.getContractFactory("Hello");
-  // deploy contracts
-  const hello  = await HELLO.deploy();
-  await hello.waitForDeployment();
-
-
-  console.log("hello deployed to: ", await hello.getAddress());
-
-  const contractAddress  = await hello.getAddress();
-
-  // Save copies of each contracts abi and address to the frontend.
-
-//  saveFrontendFiles(marketplace , "Marketplace");
-  saveFrontendFiles(contractAddress  , "Hello");
+    const HELLO  = await hre.ethers.getContractFactory("Hello");
+    // deploy contracts
+    const hello  = await HELLO.deploy();
+    await hello.waitForDeployment();
+    console.log("hello deployed to: ", await hello.getAddress());
+    const contractAddress  = await hello.getAddress();
+    saveFrontendFiles(contractAddress  , "Hello");
 }
 
 function saveFrontendFiles(contractAddress, name) {
